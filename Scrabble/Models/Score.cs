813d28d4dcdac64f7b2e9d5e736cb877434c0ea4
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-07-15 11:32:09 -0700

    refactor logic
    Co-Authored-By: Godfrey Owidi <godfreyowiidi@gmail.com>

--- Scrabble/Models/Score.cs
+++ Scrabble/Models/Score.cs
@@ -9,48 +9,62 @@ namespace Scrabble.Models
     {
       //string[] chars = word.Split('');
       int result = 0; 
-      foreach(char letter in word)
+      List<char> onePoint = new List<char>{'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't'}; 
+      List<char> twoPoints = new List<char>{'d', 'g'};
+      List<char> threePoints = new List<char>{'b', 'c', 'm', 'p'};
+      List<char> fourPoints = new List<char>{'f', 'h', 'v', 'w', 'y'};
+      List<char> fivePoints = new List<char>{'k'};
+      List<char> eightPoints = new List<char>{'j', 'x'};
+      List<char> tenPoints = new List<char>{'q', 'z'};
+
+      foreach(char letter in word) 
+      {
+        if (onePoint.Contains(letter)) result += 1;
+        else if (twoPoints.Contains(letter)) result += 2;
+        else if(threePoints.Contains(letter)) result += 3;
+        else if(fourPoints.Contains(letter)) result += 4;
+        else if(fivePoints.Contains(letter)) result += 5;
+        else if(eightPoints.Contains(letter)) result += 8;
+        else result += 10;
+      }
+      return result;
+
+      /*
+      foreach(char letter in word) //Contains
       {
         Console.WriteLine($"<{letter}>"); 
         if(letter == 'a' || letter ==  'e'|| letter ==  'i'|| letter ==  'o'|| letter ==  'u'|| letter ==  'l'|| letter ==  'n'|| letter ==  'r'|| letter ==  's'|| letter ==  't') 
         {
           result += 1;
-          Console.WriteLine(result);
         }
         else if(letter == 'd' || letter == 'g')
         {
           result += 2;
-          Console.WriteLine(result);
         }
         else if(letter == 'b' || letter == 'c' || letter == 'm' || letter == 'p')
         {
           result += 3;
-          Console.WriteLine(result);
         }
         else if(letter == 'f' || letter == 'h' || letter == 'v' || letter == 'w' || letter == 'y')
         {
           result += 4;
-          Console.WriteLine(result);
         }
         else if(letter == 'k')
         {
           result += 5;
-          Console.WriteLine(result);
         }
         else if(letter == 'j' || letter == 'x')
         {
           result += 8;
-          Console.WriteLine(result);
         }
         else if(letter == 'q'|| letter == 'z')
         {
           result += 10;
-          Console.WriteLine(result);
         }
       
       }
       return result;
-      
+      */
       
     }
 
