72a8a199dd5aa985a7f56b03a79d53dba0daa4e0
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-07-15 09:39:42 -0700

    add folder Scrabble
    Co-Authored-By: Godfrey Owidi <godfreyowiidi@gmail.com>

---
+++ Scrabble/Models/Score.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace Scrabble.Models
+{
+  public class Score
+  {
+    public static int GetScore (string word)
+    {
+      return 1;
+    }
+
+    // properties, methods, etc. will go here.
+
+  }
+}
